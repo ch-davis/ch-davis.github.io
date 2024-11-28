6eeca7e978cc3c63e7578e37cf1d723622a25c24
ch-davis <149125988+ch-davis@users.noreply.github.com>
2024-11-29 07:11:55 +0800

    Update sidebar.ts

--- docs/.vuepress/sidebar.ts
+++ docs/.vuepress/sidebar.ts
@@ -3,6 +3,8 @@ import { sidebar } from "vuepress-theme-hope";
 // 图标：https://theme-hope.vuejs.press/zh/guide/interface/icon.html#%E8%AE%BE%E7%BD%AE%E5%9B%BE%E6%A0%87
 // https://fontawesome.com/search?m=free&o=r
 export default sidebar({
+  // 新添加的注释掉内容
+  /*
   "": [
     "/DailyRoutine",
     "/Fitness",
@@ -88,11 +90,11 @@ export default sidebar({
       collapsible: true,
       children: "structure",
     },
-  ],
+  ],*/
   // 专题区（独立侧边栏）
   "/apps/topic/": "structure",
   // 如果你不想使用默认侧边栏，可以按照路径自行设置。但需要去掉下方配置中的注释，以避免博客和时间轴出现异常。_posts 目录可以不存在。
-  /*"/_posts/": [
+  "/_posts/": [
     {
       text: "博客文章",
       icon: "fa6-solid:feather-pointed",
@@ -101,5 +103,5 @@ export default sidebar({
       collapsible: true,
       children: "structure",
     },
-  ], */
+  ],
 });
